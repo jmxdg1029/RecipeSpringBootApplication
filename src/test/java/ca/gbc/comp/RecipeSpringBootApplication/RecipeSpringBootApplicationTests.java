dd1c3e2af57f0c5521a0eaa44e91d2049b141caa
jmxdg1029 <73676532+jmxdg1029@users.noreply.github.com>
2021-11-03 14:38:02 -0400

    Initial commit

---
+++ src/test/java/ca/gbc/comp/RecipeSpringBootApplication/RecipeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp.RecipeSpringBootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
